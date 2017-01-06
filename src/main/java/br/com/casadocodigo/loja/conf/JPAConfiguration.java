5fa40c93921ae3d4b81b3e63025ea96929a494c3
Patricia do Amaral <patriciaagm@gmail.com>
2017-01-06 19:32:22 -0200

    adicionando properties para configuração do banco de dados

--- src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -1,9 +1,10 @@
 package br.com.casadocodigo.loja.conf;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -19,27 +20,39 @@ public class JPAConfiguration {
 
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+		
 		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
 	
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	
 		factoryBean.setJpaVendorAdapter(vendorAdapter);
 		
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUsername("root");
-		dataSource.setPassword("root");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		Properties properties = new Properties();
 
-		factoryBean.setDataSource(dataSource);
+		//InputStream is = getClass().getResourceAsStream("../../../../../config.properties");
+		InputStream is = getClass().getResourceAsStream("../../../../../config.properties");
 		
-		Properties props = new Properties();
-		props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-		props.setProperty("hibernate.show_sql", "true");// mostrará no console as queries.
-		props.setProperty("hibernate.hbm2ddl.auto", "update"); //Toda vez que mudar o modelo, o hibernate atualizará o banco.
+		try {
+			properties.load(is);
+			
+			DriverManagerDataSource dataSource = new DriverManagerDataSource();
+			dataSource.setUsername(properties.getProperty("dataSource.username"));
+			dataSource.setPassword(properties.getProperty("dataSource.password"));
+			dataSource.setUrl(properties.getProperty("dataSource.url"));
+			dataSource.setDriverClassName(properties.getProperty("dataSource.driverClassName"));
+
+			factoryBean.setDataSource(dataSource);		
+		
+			factoryBean.setJpaProperties(properties);
+			
+			factoryBean.setPackagesToScan("br.com.casadocodigo.loja.models"); /*Scanear as entidades*/
+			
+		} catch (IOException e) {
+			//Tratar exception
+			e.printStackTrace();
+		}
+						
 	
-		factoryBean.setJpaProperties(props);
-		
-		factoryBean.setPackagesToScan("br.com.casadocodigo.loja.models"); /*Scanear as entidades*/
 		
 		return  factoryBean;
 	}
